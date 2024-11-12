381f3d5d4b6d086180fbc71e0359bf4e520f534d
Alvaro Ramos <alvaroramosandler@hotmail.com>
2024-11-11 21:39:17 -0300

    Pequeño Fix

--- malla_ulegos/src/components/MallaTable.jsx
+++ malla_ulegos/src/components/MallaTable.jsx
@@ -58,6 +58,7 @@ function MallaTable() {
             style={subject ? getCellStyle(subject) : {}}
             onClick={() => {
               setSelectedSubject(subject);
+              setHoveredSubject(null);
             }}
             onMouseEnter={() => setHoveredSubject(subject)}
           >
